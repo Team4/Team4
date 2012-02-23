b454574114a1fbd0e250cb3cd141613145255305
eDex <rsfeinst@indiana.edu>
2012-02-22 22:01:12 -0500

    Raffi First Commit

---
+++ getIt/getIt/databaseAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  databaseAppDelegate.h
+//  getIt
+//
+//  Created by Raphael Feinstein on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class databaseViewController;
+
+@interface databaseAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) databaseViewController *viewController;
+
+@end
---
+++ getIt/getIt/databaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  databaseViewController.h
+//  getIt
+//
+//  Created by Raphael Feinstein on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface databaseViewController : UIViewController
+
+@end
